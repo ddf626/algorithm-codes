a80defa9ce7a03ebe79ee40de1dc60afad7ce706
coder-ddf <436066640@qq.com>
2023-12-04 16:46:52 +0800

    readme

---
+++ algorithm-java/src/ddf/p07_stack/C24_LargestRectangleArea.java
@@ -0,0 +1,16 @@
+package ddf.p07_stack;
+
+/**
+ * <a href="https://leetcode.cn/problems/largest-rectangle-in-histogram/description">leetcode 84.柱状图中最大的矩形</a>
+ *
+ * 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+ *
+ * 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+ */
+public class C24_LargestRectangleArea {
+
+    public int largestRectangleArea(int[] heights) {
+        return -1;
+    }
+
+}
