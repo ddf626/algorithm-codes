ed1544d58ae7c7342ca35bb8c274d25672b33b92
coder-ddf <436066640@qq.com>
2024-01-02 14:54:00 +0800

    BST

--- algorithm-java/src/ddf/p11_bst/BSTreeNode.java
+++ algorithm-java/src/ddf/p11_bst/BSTreeNode.java
@@ -2,13 +2,13 @@ package ddf.p11_bst;
 
 public class BSTreeNode {
     // 结点值
-    int val;
+    public int val;
     // 左孩子指针
-    BSTreeNode left;
+    public BSTreeNode left;
     // 右孩子指针
-    BSTreeNode right;
+    public BSTreeNode right;
     // 父结点指针
-    BSTreeNode p;
+    public BSTreeNode p;
 
     public BSTreeNode() {}
 
--- algorithm-java/src/ddf/p11_bst/C54_BSTree.java
+++ algorithm-java/src/ddf/p11_bst/C54_BSTree.java
@@ -1,5 +1,10 @@
 package ddf.p11_bst;
 
+import utils.DUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class C54_BSTree {
 
     /**
@@ -128,11 +133,233 @@ public class C54_BSTree {
 
         if (val < p.val) {
             p.left = new BSTreeNode(val);
+            p.left.p = p;
             return root;
         }
 
         p.right = new BSTreeNode(val);
+        p.right.p = p;
         return root;
+    }
+
+    /**
+     * 使用结点n替换掉结点o的位置，但未处理n的孩子们
+     */
+    public BSTreeNode transplant(BSTreeNode root, BSTreeNode o, BSTreeNode n) {
+        if (o.p == null) {
+            // 说明o是根结点，直接用n把root给覆盖掉
+            root = n;
+        } else if (o == o.p.left) {
+            // o是它父亲的左子树
+            o.p.left = n;
+        } else {
+            // o是它父亲的右子树
+            o.p.right = n;
+        }
+
+        if (n != null) {
+            n.p = o.p;
+        }
+
+        return root;
+    }
+
+    /**
+     * 在以root为根结点的树中删除结点z
+     */
+    public BSTreeNode delete(BSTreeNode root, BSTreeNode z) {
+        if (z.left == null) {
+            root = transplant(root, z, z.right);
+            return root;
+        }
+
+        if (z.right == null) {
+            root = transplant(root, z, z.left);
+            return root;
+        }
+
+        // 左右子树都不为空
+
+        // 找到z的后继结点
+        BSTreeNode y = searchMin(z.right);
+        if (y.p != z) {
+            // z的后继是否是z的右孩子，如果不是，需要将y的右孩子替换y，再将y替换z
+            root = transplant(root, y, y.right);
+            y.right = z.right;
+            y.right.p = y;
+        }
+
+        // 使用y替换z
+        root = transplant(root, z, y);
+        y.left = z.left;
+        y.left.p = y;
+
+        return root;
+    }
+
+    public BSTreeNode buildTree(int[] arr) {
+        if (arr == null || arr.length < 1) {
+            return null;
+        }
+
+        BSTreeNode root = new BSTreeNode(arr[0]);
+        for (int i=1; i<arr.length; i++) {
+            insert(root, arr[i]);
+        }
+
+        return root;
+    }
+
+    public Integer randomValInTree(BSTreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inOrder(root, list);
+
+        int random = DUtils.random(0, list.size() - 1);
+        return list.get(random);
+    }
+
+    private void inOrder(BSTreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left, list);
+        list.add(node.val);
+        inOrder(node.right, list);
+    }
+
+    public boolean isValidBST(BSTreeNode root) {
+        return process(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean process(BSTreeNode root, Long low, Long high) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.val < low || root.val > high) {
+            return false;
+        }
+
+        return process(root.left, low, (long) root.val) && process(root.right, (long) root.val, high);
+    }
+
+    public int maxDepth(BSTreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public static void main(String[] args) {
+        C54_BSTree s = new C54_BSTree();
+
+//        int[] arr = new int[] {-3, -2};
+//        BSTreeNode root = s.buildTree(arr);
+//        System.out.println(s.isValidBST(root) + " 1");
+//        root = s.delete(root, s.searchRecur(root, -2));
+//        System.out.println(s.isValidBST(root) + " 2");
+//        root = s.insert(root, -1);
+//        System.out.println(s.isValidBST(root) + " 3");
+//        root = s.insert(root, -1);
+//        System.out.println(s.isValidBST(root) + " 4");
+//        root = s.insert(root, -1);
+//        System.out.println(s.isValidBST(root) + " 5");
+//        root = s.insert(root, -2);
+//        System.out.println(s.isValidBST(root) + " 6");
+//        root = s.delete(root, s.searchRecur(root, -2));
+//        System.out.println(s.isValidBST(root) + " 7");
+//        root = s.delete(root, s.searchRecur(root, -1));
+//        System.out.println(s.isValidBST(root) + " 8");
+//        root = s.delete(root, s.searchRecur(root, -1));
+//        System.out.println(s.isValidBST(root) + " 9");
+//        root = s.insert(root, -4);
+//        System.out.println(s.isValidBST(root) + " 10");
+//        root = s.delete(root, s.searchRecur(root, -3));
+//        System.out.println(s.isValidBST(root) + " 11");
+
+
+
+        int times = 50000;
+        int maxLength = 1000;
+
+        boolean ok = true;
+        for (int i=0; i<times; i++) {
+            int[] arr = DUtils.randomArr(maxLength, Integer.MIN_VALUE, Integer.MAX_VALUE);
+            List<Op> ops = new ArrayList<>();
+
+            BSTreeNode root = s.buildTree(arr);
+            if (!s.isValidBST(root)) {
+                ok = false;
+                System.out.println("ops:" + ops);
+                break;
+            }
+
+            int inTimes = 1000;
+            int insertTimes = 0;
+            int delTimes = 0;
+            for (int j=0; j<inTimes; j++) {
+                int random = DUtils.random(0, 1);
+                if (random == 0 || root == null) {
+                    int val = DUtils.random(Integer.MIN_VALUE, Integer.MAX_VALUE);
+                    ops.add(new Op(0, val));
+                    root = s.insert(root, val);
+                    insertTimes++;
+                } else {
+                    Integer val = s.randomValInTree(root);
+                    BSTreeNode bsTreeNode = s.searchRecur(root, val);
+                    if (bsTreeNode.val != val) {
+                        ok = false;
+                        break;
+                    }
+
+                    ops.add(new Op(1, val));
+                    root = s.delete(root, bsTreeNode);
+                    delTimes++;
+                }
+
+                if (!s.isValidBST(root)) {
+                    ok = false;
+                    System.out.println("ops:" + ops);
+                    break;
+                }
+
+            }
+
+            if (!ok) {
+                break;
+            } else {
+                System.out.printf("第%d次测试：BST高度:%d, insertTimes:%d, delTimes:%d \n", i, s.maxDepth(root), insertTimes, delTimes);
+            }
+        }
+
+        if (ok) {
+            System.out.println("ok! ");
+        } else {
+            System.out.println("err! ");
+        }
+    }
+
+    private static class Op {
+
+        // 0: 插入；1: 删除
+        int op;
+
+        int val;
+
+        public Op(int op, int val) {
+            this.op = op;
+            this.val = val;
+        }
+
+        @Override
+        public String toString() {
+            return "Op{" +
+                    "op=" + op +
+                    ", val=" + val +
+                    '}';
+        }
     }
 
 }
